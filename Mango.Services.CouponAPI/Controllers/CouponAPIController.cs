f5538ce13eabcf559f1ca80bdb22d25a143163b5
Vorpark <nik.vorpark@mail.ru>
2024-01-27 17:50:16 +0300

    Первичные реализации запросов Get и Get(id) в CouponAPI

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,50 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponAPIController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception)
+            {
+
+            }
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon objList = _db.Coupons.Find(id);
+                return objList;
+            }
+            catch (Exception)
+            {
+
+            }
+            return null;
+        }
+    }
+}
