4eb645e569905944a55e35fb3f3636d9d441357f
Vorpark <nik.vorpark@mail.ru>
2024-01-28 11:25:16 +0300

    Реализация интерфейса ICouponService в Mango.Web

---
+++ Mango.Web/Models/CouponDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Web.Models
+{
+    public class CouponDTO
+    {
+        public int Id { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmout { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
---
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDTO?> GetAllCouponsAsync();
+        Task<ResponseDTO?> GetCouponByIdAsync(int id);
+        Task<ResponseDTO?> GetCouponByCodeAsync(string couponCode);
+        Task<ResponseDTO?> CreateCouponAsync(CouponDTO couponDTO);
+        Task<ResponseDTO?> UpdateCouponAsync(CouponDTO couponDTO);
+        Task<ResponseDTO?> DeleteCouponAsync(int id);
+    }
+}
