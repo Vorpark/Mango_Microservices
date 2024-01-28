ffd317ca601c8344ea0b20863047043b169cab8f
Vorpark <nik.vorpark@mail.ru>
2024-01-28 11:15:07 +0300

    Реализация сервиса отправки и получения api - BaseService в Mango.Web

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,82 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ResponseDTO?> SendAsync(RequestDTO requestDTO)
+        {
+            try
+            {
+                HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "/application/json");
+                //token
+
+
+                message.RequestUri = new Uri(requestDTO.Url);
+                if (requestDTO.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDTO.Data), Encoding.UTF8, "/application/json");
+                }
+
+                HttpResponseMessage? apiResponse = null;
+
+                switch (requestDTO.ApiType)
+                {
+                    case ApiType.GET:
+                        message.Method = HttpMethod.Get;
+                        break;
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                    case HttpStatusCode.Forbidden:
+                        return new() { IsSuccess = false, Message = "Access Denied" };
+                    case HttpStatusCode.Unauthorized:
+                        return new() { IsSuccess = false, Message = "Unauthorized" };
+                    case HttpStatusCode.InternalServerError:
+                        return new() { IsSuccess = false, Message = "Internal Server Error" };
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var apiResponseDTO = JsonConvert.DeserializeObject<ResponseDTO>(apiContent);
+                        return apiResponseDTO;
+                }
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDTO()
+                {
+                    IsSuccess = false,
+                    Message = ex.Message.ToString()
+                };
+                return dto;
+            }
+        }
+    }
+}
---
+++ Mango.Web/Service/IService/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface IBaseService
+    {
+       Task<ResponseDTO?> SendAsync(RequestDTO requestDTO);
+    }
+}
